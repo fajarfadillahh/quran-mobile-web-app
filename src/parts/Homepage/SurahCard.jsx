5e0583710cf5fec476ff5625a78753f1fdac1558
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-23 14:07:26 +0700

    fixed: SurahCard - Homepage parts

--- src/parts/Homepage/SurahCard.jsx
+++ src/parts/Homepage/SurahCard.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const SurahCard = ({ surah }) => {
   return (
-    <div className="flex min-h-[72px] items-center justify-between rounded-md bg-white px-4 shadow-lg transition hover:ring-2 hover:ring-green-600">
+    <div className="flex min-h-[72px] items-center justify-between rounded-md bg-white p-4 shadow-lg transition hover:ring-2 hover:ring-green-600">
       <div className="inline-flex items-center gap-4">
-        <span className="inline-flex h-8 w-8 items-center justify-center rounded-full bg-green-600 font-bold text-white">
+        <span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-green-600 text-[14px] font-bold text-white">
           {surah.nomor_surah}
         </span>
 
-        <div className="max-w-[160px] overflow-hidden">
+        <div className="max-w-[180px] overflow-hidden">
           <div className="flex items-center gap-1">
             <h3 className="section-title whitespace-nowrap">
               {surah.nama_latin}
