4686b41195ccc325e9f012d6e153ae528bab3f06
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-23 20:44:36 +0700

    fixed: Footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-100">
+    <footer className="main min-h-max">
       <div className="container border-t-2 border-gray-300 py-8 text-center">
         <h1 className="font-bold text-gray-900">
           @ Copyrights 2023 - Develop by ❤️
