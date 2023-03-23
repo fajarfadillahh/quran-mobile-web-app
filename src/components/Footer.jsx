cc7f183d843212ab5d10a9647e87a5e01eb1ee64
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-23 22:46:35 +0700

    modified: dark mode Footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="main min-h-max">
-      <div className="container border-t-2 border-gray-300 py-8 text-center">
-        <h1 className="font-bold text-gray-900">
+      <div className="container border-t-2 border-gray-300 py-8 text-center dark:border-t-gray-800">
+        <h1 className="font-bold text-gray-900 dark:text-white">
           @ Copyrights 2023 - Develop by ❤️
         </h1>
       </div>
