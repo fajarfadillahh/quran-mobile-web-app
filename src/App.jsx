a97a3eb859c9522032cfca6df603a548cbd5d2bf
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-24 13:09:51 +0700

    create: Surah page & Hero - Surah page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // import pages
 import Home from "./pages/Home";
+import Surah from "./pages/Surah";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/surah/:id" element={<Surah />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Surah.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// import components
+import Header from "../components/Header";
+
+// import parts
+import Hero from "../parts/Surahpage/Hero";
+
+const Surah = () => {
+  return (
+    <>
+      <Header />
+      <main className="main">
+        <Hero />
+      </main>
+    </>
+  );
+};
+
+export default Surah;
