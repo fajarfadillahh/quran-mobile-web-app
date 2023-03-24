6cdb52ca1f94eb3f583b7593843b6e520ea62a09
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-24 22:07:38 +0700

    deleted: Hero - Surah page | modified: SurahList - Surah page

--- src/pages/Surah.jsx
+++ src/pages/Surah.jsx
@@ -6,7 +6,6 @@ import Footer from "../components/Footer";
 import ScrollToTop from "../components/ScrollToTop";
 
 // import parts
-import Hero from "../parts/Surahpage/Hero";
 import SurahList from "../parts/Surahpage/SurahList";
 
 const Surah = () => {
@@ -14,7 +13,6 @@ const Surah = () => {
     <>
       <Header />
       <main className="main">
-        <Hero />
         <SurahList />
       </main>
       <Footer />
--- src/parts/Surahpage/SurahList.jsx
+++ src/parts/Surahpage/SurahList.jsx
@@ -22,7 +22,19 @@ const SurahList = () => {
   }, [id]);
 
   return (
-    <section className="section pt-12">
+    <section className="grid gap-12 pb-16">
+      <div className="bg-gradient-to-tl from-green-600 to-green-500 pt-32 pb-12">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="font-arabic text-[84px] font-bold text-white">
+            {surah.arabic}
+          </h1>
+          <h3 className="-mt-7 text-[28px] font-extrabold text-white">
+            {surah.latin}
+          </h3>
+          <p className="font-semibold text-white">({surah.translation})</p>
+        </div>
+      </div>
+
       <div className="container">
         <div className="grid gap-8">
           {surah.ayat?.map((surah, index) => {
