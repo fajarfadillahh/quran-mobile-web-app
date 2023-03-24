e691959d2db000038c0df9355515eb018b22e39c
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-24 13:19:13 +0700

    modified: Hero - Surah page

--- src/parts/Surahpage/Hero.jsx
+++ src/parts/Surahpage/Hero.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero - Surah page</div>;
+  return (
+    <section className="section bg-gradient-to-tl from-green-600 to-green-500 pt-32">
+      <div className="container">
+        <div className=" flex flex-col items-center justify-center">
+          <h1 className="font-arabic text-[84px] font-bold text-white">
+            الفاتحة
+          </h1>
+          <h3 className="-mt-7 text-[28px] font-extrabold text-white">
+            Al-Fatihah
+          </h3>
+          <p className="font-semibold text-white">(Pembuka)</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
