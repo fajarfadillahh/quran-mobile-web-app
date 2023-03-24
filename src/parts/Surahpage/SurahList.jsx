fa0be64d7b736651137681b952b3628451f50807
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-24 13:28:28 +0700

    tracked: SurahList - Surah page

---
+++ src/parts/Surahpage/SurahList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SurahList = () => {
+  return (
+    <section className="section pt-12">
+      <div className="container">
+        <div className="grid justify-center gap-8">
+          <h2>Ayat surah</h2>
+          <h2>Ayat surah</h2>
+          <h2>Ayat surah</h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SurahList;
