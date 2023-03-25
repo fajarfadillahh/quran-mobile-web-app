533020c5897115f8eda9e88547b01090c9fbe3f8
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-25 21:48:57 +0700

    modified: fixed change position Header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,46 +6,20 @@ import { RiMoonClearFill, RiSunFill } from "react-icons/ri";
 import useColorMode from "../hooks/useColorMode";
 
 const Header = () => {
-  const [stickyHeader, setStickyHeader] = useState(false);
-  const [headerBgColor, setHeaderBgColor] = useState("bg-transparent");
-  const [textColor, setTextColor] = useState("text-white");
   const [colorMode, setColorMode] = useColorMode();
 
-  // handle scrolling for sticky header
-  const handleScroll = () => {
-    if (window.scrollY > 10) {
-      setStickyHeader(true);
-      setHeaderBgColor("bg-white shadow-lg dark:bg-gray-900");
-      setTextColor("text-gray-900 dark:text-white");
-    } else {
-      setStickyHeader(false);
-      setHeaderBgColor("bg-transparent shadow-none");
-      setTextColor("text-white");
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [stickyHeader]);
-
   return (
-    <header
-      className={`fixed inset-x-0 top-0 z-50 mx-auto max-w-[480px] transition ${headerBgColor}`}
-    >
+    <header className="absolute inset-x-0 top-0 z-50 mx-auto max-w-[480px] bg-transparent transition">
       <div className="container flex h-24 items-center justify-between">
         <Link
           to="/"
-          className={`text-[24px] font-extrabold transition ${textColor}`}
+          className="text-[24px] font-extrabold text-white transition"
         >
           Qur'an.
         </Link>
 
         <div
-          className={`inline-flex cursor-pointer items-center gap-1 transition ${textColor}`}
+          className="inline-flex cursor-pointer items-center gap-1 text-white transition"
           onClick={() => setColorMode(colorMode === "light" ? "dark" : "light")}
         >
           {colorMode === "light" ? (
