d74a321fddfdb8225ffce58d643e0610f97cfe79
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-25 21:30:27 +0700

    modified: fixed SurahAyat parts - Surah page

--- src/parts/Surahpage/SurahAyat.jsx
+++ src/parts/Surahpage/SurahAyat.jsx
@@ -4,10 +4,10 @@ const SurahAyat = ({ surah }) => {
   return (
     <div className="grid gap-3">
       <div className="flex items-start justify-between gap-4">
-        <span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-green-600 text-[14px] font-bold text-white">
+        <span className="inline-flex h-[40px] w-[40px] items-center justify-center rounded-full bg-green-600 text-[14px] font-bold text-white">
           {surah.ayat_number}
         </span>
-        <h1 className="max-w-[330px] text-right text-[32px] font-bold text-gray-900 dark:text-white">
+        <h1 className="max-w-[330px] text-right text-[28px] font-bold text-gray-900 dark:text-white">
           {surah.arabic}
         </h1>
       </div>
