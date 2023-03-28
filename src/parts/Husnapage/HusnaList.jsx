1c50eb6d5feba7ebd84f0258267dc64821b1b7c0
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-28 22:34:52 +0700

    create: HusnaCard parts - Husnapage

--- src/parts/Husnapage/HusnaList.jsx
+++ src/parts/Husnapage/HusnaList.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+// import parts
+import HusnaCard from "./HusnaCard";
+
 const HusnaList = () => {
   return (
     <section className="section pt-12">
-      <div className="container grid grid-cols-2 gap-12">
-        <h1>husna card mapping</h1>
-        <h1>husna card mapping</h1>
+      <div className="container grid grid-cols-2 gap-8">
+        <HusnaCard />
+        <HusnaCard />
       </div>
     </section>
   );
