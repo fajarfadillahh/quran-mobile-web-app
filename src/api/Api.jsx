fbe4cbf28e5697eb9f6c75c693a7d1e7fe3cd4fc
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-28 22:39:36 +0700

    modified: get asmaul-husna data from api

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,25 +1,31 @@
 import axios from "axios";
 
-const RESTSurah = axios.create({
+const URL = axios.create({
   baseURL: "http://apimuslimify.vercel.app/api/v2",
 });
 
 // get all surah
 const getAllSurah = async () => {
-  const response = await RESTSurah.get(`/surah`);
+  const response = await URL.get(`/surah`);
   return response.data.data;
 };
 
 // get surah by id
 const getSurahById = async (id) => {
-  const response = await RESTSurah.get(`/surah?number=${id}`);
+  const response = await URL.get(`/surah?number=${id}`);
   return response.data.data;
 };
 
 // get quote
 const getQuote = async () => {
-  const response = await RESTSurah.get(`/quote`);
+  const response = await URL.get(`/quote`);
   return response.data.data;
 };
 
-export { getAllSurah, getSurahById, getQuote };
+// get asmaul-husna
+const getAsmaulHusna = async () => {
+  const response = await URL.get(`/asmaulhusna`);
+  return response.data.data;
+};
+
+export { getAllSurah, getSurahById, getQuote, getAsmaulHusna };
--- src/pages/Husna.jsx
+++ src/pages/Husna.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+// import api
+import { getAsmaulHusna } from "../api/Api";
 
 // import components
 import Layout from "../components/Layout";
@@ -6,6 +9,12 @@ import Hero from "../parts/Husnapage/Hero";
 import HusnaList from "../parts/Husnapage/HusnaList";
 
 const Husna = () => {
+  const [asmaulHusna, setAsmaulHusna] = useState([]);
+
+  useEffect(() => {
+    getAsmaulHusna();
+  }, []);
+
   return (
     <Layout>
       <Hero />
