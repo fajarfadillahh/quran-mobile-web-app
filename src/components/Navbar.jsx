519a3a5f5397d303a188ba3cbc3e93becab0eb54
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-29 23:25:05 +0700

    modified: fixed Navbar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FaQuran, FaPrayingHands, FaStarAndCrescent } from "react-icons/fa";
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav className="fixed inset-x-0 bottom-0 z-50 mx-auto flex h-[84px] max-w-[480px] items-center justify-center bg-white shadow-[0_-6px_15px_rgba(0,0,0,0.1)] transition dark:bg-gray-900">
       <div className="container">
@@ -13,14 +15,19 @@ const Navbar = () => {
             [<FaStarAndCrescent />, "Husna", "/asmaul-husna"],
           ].map(([icon, text, link]) => {
             return (
-              <NavLink
+              <Link
                 to={link}
                 key={text}
-                className="navbar-active inline-flex flex-col items-center gap-1 text-gray-600 dark:text-gray-500"
+                className={`navbar-active inline-flex flex-col items-center gap-1 text-gray-600 dark:text-gray-500 ${
+                  location.pathname === link ||
+                  (location.pathname.startsWith("/surah/") && link === "/")
+                    ? "active"
+                    : null
+                }`}
               >
                 <span className="text-[1.5rem]">{icon}</span>
                 <p className="text-[14px] font-extrabold">{text}</p>
-              </NavLink>
+              </Link>
             );
           })}
         </ul>
