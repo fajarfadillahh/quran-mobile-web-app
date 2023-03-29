655c46d6af7702fe9b55d46a1bd6c8837d64fbb5
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-29 23:07:27 +0700

    modified: dark mode HusnaCard parts - Husnapage

--- src/parts/Husnapage/HusnaCard.jsx
+++ src/parts/Husnapage/HusnaCard.jsx
@@ -4,7 +4,7 @@ const HusnaCard = ({ asmaulHusna }) => {
   const { number, arabic, latin, id_translation } = asmaulHusna;
 
   return (
-    <div className="group h-max w-[175px] rounded-md bg-white shadow-md transition hover:ring-2 hover:ring-green-600">
+    <div className="group h-max w-[175px] rounded-md bg-white shadow-md transition hover:ring-2 hover:ring-green-600 dark:bg-gray-800">
       <div className="grid gap-4 p-6">
         <span className="inline-flex h-[28px] w-[28px] items-center justify-center rounded-full bg-green-600 text-[12px] font-bold text-white">
           {number}
@@ -13,7 +13,7 @@ const HusnaCard = ({ asmaulHusna }) => {
         <div className="grid gap-2 text-center">
           <h1
             dir="rtl"
-            className="truncate text-[28px] font-bold text-gray-900 rtl:mr-0"
+            className="truncate text-[28px] font-bold text-gray-900 rtl:mr-0 dark:text-white"
           >
             {arabic}
           </h1>
@@ -22,7 +22,7 @@ const HusnaCard = ({ asmaulHusna }) => {
             <h3 className="truncate whitespace-nowrap text-[14px] font-extrabold text-green-600">
               {latin}
             </h3>
-            <p className="text-[12px] text-gray-600 line-clamp-2 group-hover:line-clamp-none">
+            <p className="text-[12px] text-gray-600 line-clamp-2 group-hover:line-clamp-none dark:text-white">
               {id_translation}
             </p>
           </div>
