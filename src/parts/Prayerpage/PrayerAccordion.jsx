c495dd969119944f4d4c130124bdec426de0c77d
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-30 20:44:22 +0700

    modified: PrayerList & PrayerAccordion parts - Prayerpage

--- src/parts/Prayerpage/PrayerAccordion.jsx
+++ src/parts/Prayerpage/PrayerAccordion.jsx
@@ -2,15 +2,24 @@ import React from "react";
 import { Collapse } from "react-collapse";
 import { RiArrowUpSLine } from "react-icons/ri";
 
-const PrayerAccordion = ({ prayer }) => {
+const PrayerAccordion = ({ prayer, isOpen, handleIsOpen }) => {
   return (
-    <div className="flex flex-col items-center justify-center rounded-md bg-white shadow-lg">
+    <div
+      className={`flex flex-col items-center justify-center rounded-md bg-white shadow-lg transition ${
+        isOpen ? "ring-2 ring-green-600" : "ring-0"
+      }`}
+      onClick={handleIsOpen}
+    >
       <div className="flex h-[72px] w-full cursor-pointer items-center justify-between px-4 text-gray-900">
         <h1 className="font-bold">{prayer.name}</h1>
-        <RiArrowUpSLine className="1.3rem" />
+        <RiArrowUpSLine
+          className={`text-[1.5rem] transition ${
+            isOpen ? "rotate-180 text-green-600" : "rotate-0"
+          }`}
+        />
       </div>
 
-      <Collapse isOpened={true}>
+      <Collapse isOpened={isOpen}>
         <div className="grid h-max w-full gap-4 py-[20px] px-4">
           <h1 className="text-right text-[24px] font-bold text-gray-900">
             {prayer.arabic}
--- src/parts/Prayerpage/PrayerList.jsx
+++ src/parts/Prayerpage/PrayerList.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 // import components
 import PrayerAccordion from "./PrayerAccordion";
 
 const PrayerList = ({ dataPrayer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleIsOpen = (index) => {
+    if (isOpen === index) {
+      return setIsOpen(null);
+    }
+
+    setIsOpen(index);
+  };
+
   return (
     <section className="section pt-12">
       <div className="container grid gap-4">
         {dataPrayer?.map((prayer, index) => {
-          return <PrayerAccordion key={index} prayer={prayer} />;
+          return (
+            <PrayerAccordion
+              key={index}
+              prayer={prayer}
+              isOpen={isOpen === index}
+              handleIsOpen={() => handleIsOpen(index)}
+            />
+          );
         })}
       </div>
     </section>
